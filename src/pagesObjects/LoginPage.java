7b2f37367f4e5058df15fca3bcb44a8f3837a8e1
Elkin Ossa <eossa93@hotmail.com>
2017-11-14 13:15:09 -0500

    Verify that is not logged

--- src/pagesObjects/LoginPage.java
+++ src/pagesObjects/LoginPage.java
@@ -10,60 +10,60 @@ import utils.ExcelUtils;
 import utils.ExcelUtils.ExcelType;
 
 public class LoginPage extends PageBase {
-	private static ExcelUtils excelUtils;
-	
-	public LoginPage(WebDriver driver, String pageTitle) {
-		super(driver, pageTitle);
-	}
+    private static ExcelUtils excelUtils;
 
-	// WebElements
-	@FindBy(how = How.NAME, using = "userName")
-	private WebElement inputUserName;
-	@FindBy(how = How.NAME, using = "password")
-	private WebElement inputPassword;
-	@FindBy(how = How.NAME, using = "login")
-	private WebElement buttonSingIn;
-	@FindBy(how = How.LINK_TEXT, using = "your destination")
-	private WebElement lnkDestination;
-	@FindBy(how = How.LINK_TEXT, using = "featured vacation destinations")
-	private WebElement lnkVacation;
-	@FindBy(how = How.LINK_TEXT, using = "Register here")
-	private WebElement lnkRegister;
+    public LoginPage(WebDriver driver, String pageTitle) {
+        super(driver, pageTitle);
+    }
 
-	public WebElement getInputUserName() {
-		return inputUserName;
-	}
+    // WebElements
+    @FindBy(how = How.NAME, using = "userName")
+    private WebElement inputUserName;
+    @FindBy(how = How.NAME, using = "password")
+    private WebElement inputPassword;
+    @FindBy(how = How.NAME, using = "login")
+    private WebElement buttonSingIn;
+    @FindBy(how = How.LINK_TEXT, using = "your destination")
+    private WebElement lnkDestination;
+    @FindBy(how = How.LINK_TEXT, using = "featured vacation destinations")
+    private WebElement lnkVacation;
+    @FindBy(how = How.LINK_TEXT, using = "Register here")
+    private WebElement lnkRegister;
 
-	public WebElement getInputPassword() {
-		return inputPassword;
-	}
+    public WebElement getInputUserName() {
+        return inputUserName;
+    }
 
-	public WebElement getButtonSingIn() {
-		return buttonSingIn;
-	}
+    public WebElement getInputPassword() {
+        return inputPassword;
+    }
 
-	public WebElement getLnkDestination() {
-		return lnkDestination;
-	}
+    public WebElement getButtonSingIn() {
+        return buttonSingIn;
+    }
 
-	public WebElement getLnkVacation() {
-		return lnkVacation;
-	}
+    public WebElement getLnkDestination() {
+        return lnkDestination;
+    }
 
-	public WebElement getLnkRegister() {
-		return lnkRegister;
-	}
-	
-	public void loginMercuryTours() {
+    public WebElement getLnkVacation() {
+        return lnkVacation;
+    }
+
+    public WebElement getLnkRegister() {
+        return lnkRegister;
+    }
+
+    public void loginMercuryTours() {
         try {
             excelUtils = new ExcelUtils("CredencialesLogin.xlsx", ExcelType.XLSX);
             String username = excelUtils.getCellData(1, 0);
             String password = excelUtils.getCellData(1, 1);
-			sendText(inputUserName, username);
-			sendText(inputPassword, password);
-			clickButtonLink(buttonSingIn);
+            sendText(inputUserName, username);
+            sendText(inputPassword, password);
+            clickButtonLink(buttonSingIn);
         } catch (Exception e) {
             e.printStackTrace();
         }
-	}
+    }
 }
--- src/test/autenticacion/LoginIncorrecto.java
+++ src/test/autenticacion/LoginIncorrecto.java
@@ -4,13 +4,23 @@ import base.TestBase;
 import org.testng.annotations.Test;
 import pagesObjects.LoginPage;
 import utils.ExcelUtils;
-import utils.ExcelUtils.ExcelType;
+
+import static org.testng.Assert.assertTrue;
 
 public class LoginIncorrecto extends TestBase {
     @Test
     public void f() {
         LoginPage login = new LoginPage(driver, "Welcome: Mercury Tours");
-        login.loginMercuryTours();
-        // TODO: Revisar que no esté logueado.
+        try {
+            excelUtils = new ExcelUtils("CredencialesLogin.xlsx", ExcelUtils.ExcelType.XLSX);
+            String username = excelUtils.getCellData(2, 0);
+            String password = excelUtils.getCellData(2, 1);
+            login.type("userName", username)
+                    .type("password", password)
+                    .click("[name='login']");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        assertTrue(driver.getTitle().contains("Sign-on: Mercury Tours"));
     }
 }
